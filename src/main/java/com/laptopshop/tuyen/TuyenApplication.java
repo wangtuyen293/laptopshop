d3029f0ba326820328064f90485172cae7c2f3a6
wangtuyen293 <quangtuyen.contact@gmail.com>
2024-12-13 22:44:28 +0700

    fisrt

---
+++ src/main/java/com/laptopshop/tuyen/TuyenApplication.java
@@ -0,0 +1,13 @@
+package com.laptopshop.tuyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuyenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laptopshop/tuyen/TuyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptopshop.tuyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
